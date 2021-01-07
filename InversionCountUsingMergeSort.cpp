8fc03aaf1c9f2135a231e8734e68f342e89099ee
shailesh437 <shaileshprasad05@gmail.com>
2021-01-07 10:00:38 +0530

    Create InversionCountUsingMergeSort.cpp
    Inversion count is a like measuring the sortedness of the given list of numbers.
    in array a[n], where 0<=i<n-1 and 1<=j<=n-1 if there is a pair a[i],a[j] such that a[i]>a[j AND i<j, then that pair contributes to count of "Inversion count".
    The above program calculates the inversion count by taking help of merge sort .

---
+++ InversionCountUsingMergeSort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int merge(int A[],int *temp,int start,int midpoint,int end){
+	int inv_count=0;
+	int left_ptr=start;
+	int right_ptr=midpoint+1;
+	int k=0;
+	while(left_ptr<=midpoint && right_ptr<=end){
+		if(A[left_ptr]<=A[right_ptr]){
+			temp[k++]=A[left_ptr++];
+		}else{
+			temp[k++]=A[right_ptr++];
+			inv_count+= midpoint - left_ptr + 1;
+		}
+	}
+
+	while(left_ptr<=midpoint)
+		temp[k++]=A[left_ptr++];
+	while(right_ptr<=end)
+		temp[k++]=A[right_ptr++];
+
+	for(int p=start;p<=end;p++)
+		A[p]=temp[p];
+	return inv_count;
+}
+int invCountByMergeSort(int A[],int *temp,int start,int end){
+	int midpoint,invCount=0;
+	if(start<end){
+		midpoint = (start+end)>>1;
+		invCount+=	invCountByMergeSort(A,temp,start,midpoint);
+		invCount+=	invCountByMergeSort(A,temp,midpoint+1,end);
+		invCount+=  merge(A,temp,start,midpoint,end);
+	}
+	return invCount;
+}
+int main(){
+	int A[]={5,3,4,2,1};
+	int *temp = new int[sizeof(A)/sizeof(A[0])];
+	cout<<invCountByMergeSort(A,temp,0,(sizeof(A)/sizeof(A[0]))-1);
+	return 0;
+}
