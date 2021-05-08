bae406ed642b92002a96f173a29f43510618d464
shailesh437 <shaileshprasad05@gmail.com>
2021-05-09 00:00:10 +0530

    Create ReverseSimple.cpp

---
+++ ReverseSimple.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    int *a = new int[N];
+    for(int i=0;i<N;i++){
+        cin>>a[i];
+    }
+    for(int i=N-1;i>=0;i--){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
