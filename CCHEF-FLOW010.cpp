4e95f0f941fea8223ab579d8bf6ed908b660d9ac
shailesh437 <shaileshprasad05@gmail.com>
2021-09-14 01:08:03 +0530

    Create CCHEF-FLOW010.cpp

---
+++ CCHEF-FLOW010.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define vi vector<ll> 
+#define pb push_back
+#define inf 1e18
+#define loop(i,a,b) for(int i=a;i<=b;i++)
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;
+using namespace std;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(){
+clock_t begin = clock();
+	file_i_o();
+	map<char,string> shipMap;
+	shipMap['B']=shipMap['b']="BattleShip";
+	shipMap['C']=shipMap['c']="Cruiser";
+	shipMap['D']=shipMap['d']="Destroyer";
+	shipMap['F']=shipMap['f']="Frigate";
+
+	int TC;
+	cin>>TC;
+	while(TC--){
+		char C;
+		cin>>C;
+		cout<<shipMap[C]<<endl;
+	}
+	 #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif 
+	return 0;
+	}
