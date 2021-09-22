beec487980add0cbdf04c9c6dacfff60424270a8
shailesh437 <shaileshprasad05@gmail.com>
2021-09-22 17:19:09 +0530

    Create CCHEF-FLOW009.cpp

---
+++ CCHEF-FLOW009.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define vi vector<ll> 
+#define pb push_back
+#define inf 1e18
+#define loop(i,a,b) for(int i=a;i<=b;i++)
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;
+using namespace std;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(){
+clock_t begin = clock();
+	file_i_o();
+	int TC;
+	cin>>TC;
+	while(TC--){
+		double QTY,PRC,TOTAL;
+		cin>>QTY>>PRC; 
+		TOTAL=QTY*PRC;
+		cout<<setprecision(10)<<fixed;
+		if(QTY>1000){ 
+			cout<<TOTAL*(0.9)<<endl;
+		}else{
+			cout<<TOTAL<<endl;
+		}
+
+	}
+	 #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif 
+	return 0;
+	}
