fb4c170795f5aef67e733a6f3c9eda14b33b4f59
shailesh437 <shaileshprasad05@gmail.com>
2022-08-22 02:19:36 +0530

    Create segregate-even-and-odd-nodes-in-a-linked-list5035.cpp

---
+++ CAPSULE/segregate-even-and-odd-nodes-in-a-linked-list5035.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+        // code here
+        queue<Node*> Q;
+        queue<Node*> O;
+        Node *temp=head;
+        while(temp!=NULL){
+            if(temp->data %2 == 0) Q.push(temp);
+            else O.push(temp);
+            temp=temp->next;
+        }
+        if(Q.empty() or O.empty()) return head;
+        head=Q.front();
+        Q.pop(); 
+        temp=head;
+        if(temp!=NULL)
+        temp->next=NULL;
+        
+        while(temp!=NULL and !Q.empty()){
+            temp->next=Q.front();
+            Q.pop();
+            temp=temp->next;
+        }
+        if(temp!=NULL){
+            temp->next=O.front();
+            O.pop();
+            temp=temp->next;
+        }
+        
+        if(temp!=NULL)
+        temp->next=NULL;
+        
+        
+      
+        while(temp!=NULL and !O.empty()){
+            temp->next=O.front();
+            O.pop();
+            temp=temp->next;
+        }  
+        if(temp!=NULL)
+        temp->next=NULL;
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+
+// } Driver Code Ends
