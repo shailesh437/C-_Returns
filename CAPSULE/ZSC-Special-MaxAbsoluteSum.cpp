ea29e29b247fe68a27757cdebf66da77b0bcbc9f
shailesh437 <shaileshprasad05@gmail.com>
2022-12-04 00:13:45 +0530

    Create ZSC-Special-MaxAbsoluteSum.cpp

---
+++ CAPSULE/ZSC-Special-MaxAbsoluteSum.cpp
@@ -0,0 +1,16 @@
+int Solution::maxArr(vector<int> &A) {
+    int ANS=INT_MIN;
+    int max1=INT_MIN;
+    int max2=INT_MIN;
+    int min1=INT_MAX;
+    int min2=INT_MAX;
+    for(int i=0;i<A.size();i++){
+        max1=max(max1,A[i]-i);
+        min1=min(min1,A[i]-i);
+        max2=max(max2,A[i]+i);
+        min2=min(min2,A[i]+i);
+
+        ANS=max(ANS,max(abs(max1-min1),abs(max2-min2)));
+    } 
+    return ANS;
+}
