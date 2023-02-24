fcd07428917a343efee90707205169e3152915c2
shailesh437 <shaileshprasad05@gmail.com>
2023-02-25 01:58:16 +0530

    Create ZSC-invertTree.cpp

---
+++ CAPSULE/ZSC-Tree-1/ZSC-invertTree.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ void invert(TreeNode * A){
+     if(!A) return ;
+    TreeNode* temp = A->left;
+    A->left=A->right;
+    A->right=temp;
+    invert(A->left);
+    invert(A->right);
+ }
+TreeNode* Solution::invertTree(TreeNode* A) {
+    invert(A);
+    return A;
+}
